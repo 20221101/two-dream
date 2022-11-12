6cffcbc9a320dbc6c75f38b9c31db7ac98b83198
20221101 <118040171+20221101@users.noreply.github.com>
2022-11-12 14:14:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,43 +8,68 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
+  APP_ID: 'wxa8e3f3f0f8fa122f',
+  
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '4e09847354a39dd4dcb6a5ba43ddcea0',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
+  PROVINCE: '河南',
+  CITY: '许昌',
+SWITCH: {
+  oneTalk: true,
+  horoscope: true,
+},
+  USERS: [{id:'oCP5y6DSfvLtkLnkTIrc8mS0ao_8',
+          useTemplateId: 'AeGLVZBwDTCuyBzTsK1Ao5FYBRVtmcVrT4bk7pDFjJY',
+          horoscopeDate: '12-27',
+           festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小尚同学', year: '1999', date: '10-07',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '*生日', name: '小李同学', year: '2000', date: '11-03',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '05-11',
+        },
+      ],
+            customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-05-11' },
+        // 结婚纪念日
+        { keyword: 'birthday_message.DATA', date: '2022-05-11' },
+      ],
+          },
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小徐同学',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oCP5y6JQXIH_c1t92JPUUOdLO5vI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'AeGLVZBwDTCuyBzTsK1Ao5FYBRVtmcVrT4bk7pDFjJY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小尚同学', year: '1999', date: '10-07',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '小李同学', year: '2000', date: '11-03',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '05-11',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-05-11' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'birthday_message.DATA', date: '2022-05-11' },
       ],
     },
   ],
@@ -64,4 +89,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
