43d38398512b740812cbf47a27594c2767a5cb32
20221101 <118040171+20221101@users.noreply.github.com>
2022-11-12 14:25:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,11 +25,11 @@ SWITCH: {
            festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小尚同学', year: '1999', date: '10-07',
+          type: '*生日', name: '小李同学', year: '2000', date: '11-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '*生日', name: '小李同学', year: '2000', date: '11-03',
+          type: '*生日', name: '小尚同学', year: '1999', date: '10-07',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '05-11',
@@ -46,7 +46,7 @@ SWITCH: {
       // 想要发送的人的名字
       name: '小尚同学',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCP5y6JQXIH_c1t92JPUUOdLO5vI',
+      id: 'oCP5y6GalMA9xzDZmWZ_WJ6BhjYM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'wVsBirQKAsVnWVuMoDZWDa_gmr1rtbbg7WUtv_bt9I0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -54,11 +54,11 @@ SWITCH: {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小尚同学', year: '1999', date: '10-07',
+          type: '*生日', name: '小李同学', year: '2000', date: '11-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '*生日', name: '小李同学', year: '2000', date: '11-03',
+          type: '*生日', name: '小尚同学', year: '1999', date: '10-07',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '05-11',
