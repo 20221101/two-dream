6f5def268d11994bd61c74a91921983fe13fb9d8
20221101 <118040171+20221101@users.noreply.github.com>
2022-11-15 20:36:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,13 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxa8e3f3f0f8fa122f',
+  APP_ID: '',
   
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '4e09847354a39dd4dcb6a5ba43ddcea0',
+  APP_SECRET: '',
 
   PROVINCE: '河南',
   CITY: '许昌',
@@ -19,8 +20,8 @@ SWITCH: {
   oneTalk: true,
   horoscope: true,
 },
-  USERS: [{id:'oCP5y6DSfvLtkLnkTIrc8mS0ao_8',
-          useTemplateId: 'wVsBirQKAsVnWVuMoDZWDa_gmr1rtbbg7WUtv_bt9I0',
+  USERS: [{id:'PDU17767TKrmbq1xMzLAE64BmqEW4DC8geB2pJN7p',
+          useTemplateId: '0001',
           horoscopeDate: '12-27',
            festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -46,9 +47,9 @@ SWITCH: {
       // 想要发送的人的名字
       name: '小尚同学',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCP5y6GalMA9xzDZmWZ_WJ6BhjYM',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wVsBirQKAsVnWVuMoDZWDa_gmr1rtbbg7WUtv_bt9I0',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
